87c07c0313329b410ea608805b0b55a7886d0afc
MadhuriKona <konamadhuri.reddy@gmail.com>
2018-12-15 19:30:18 +0530

    Revert "testing revert"
    This reverts commit a2846175e4c6eac9f345123f1a86ef263e8ff353.

--- dbconnect.java
+++ dbconnect.java
@@ -1,3 +1,2 @@
 this is one morew test file for testing branches
-Hope you should succeed in this
-testing reset command.
+Hope you should succeed in this.
--- userbean.java
+++
@@ -1,2 +0,0 @@
-Testing revert command
-Revert command is user to retrieve back changes from remote repository.
